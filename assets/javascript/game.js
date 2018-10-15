cd0c13cc7cbd1ffeb089e118d1e92b993f4be04f
echavez85 <echavez85@gmail.com>
2018-10-14 21:01:23 -0500

    Made updates to html and js.

---
+++ assets/javascript/game.js
@@ -0,0 +1,20 @@
+var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+var wins = 0;
+var losses = 0;
+var guessesLeft = 9;
+var lettersGuessed = [];
+var computerGuess = [];
+
+window.onload = function() {
+    var compGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    computerGuess.push(compGuess);
+    console.log(computerGuess[0]);
+}
+
+document.onkeyup = function(event) {
+    var playerGuess = event.key;
+    lettersGuessed.push(playerGuess);
+    console.log(computerGuess[0]);
+
+}
