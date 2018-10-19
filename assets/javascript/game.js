acb8dabd3cb7cbf014f1d4892dbcaa59d6914553
echavez85 <echavez85@gmail.com>
2018-10-19 18:06:45 -0500

    Updated JS comments

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,6 @@
+// This will be the choices available to the computer.
 var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
 
 var wins = 0;
 var losses = 0;
